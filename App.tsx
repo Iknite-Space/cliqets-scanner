ada06fde051b95461fe40b7b6e0667205c2febfe
Fongoh Martin T <echelon-zeus@Mons-MacBook-Pro.local>
2023-05-17 09:31:26 +0100

    Installed permission checker

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import {NativeBaseProvider, extendTheme} from 'native-base';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {request, check, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
 import MainRouter from './src/navigation';
+import {Alert, PermissionStatus, Platform} from 'react-native';
 
 const newColorTheme = {
   primary: {
@@ -18,6 +20,22 @@ const newColorTheme = {
 const theme = extendTheme({colors: newColorTheme});
 
 function App(): JSX.Element {
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      check(PERMISSIONS.IOS.CAMERA).then((res: PermissionStatus) => {
+        console.log('here', {res});
+        if (res === RESULTS.GRANTED) {
+          return;
+        }
+        console.log({res});
+
+        request(PERMISSIONS.IOS.CAMERA).then((r: PermissionStatus) => {
+          Alert.alert('Permission status', r);
+        });
+      });
+    }
+  }, []);
+
   return (
     <NativeBaseProvider theme={theme}>
       <MainRouter />
